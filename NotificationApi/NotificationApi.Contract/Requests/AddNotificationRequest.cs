1bb73e1083f6f5d9e71f9f22bccd5775ed105740
virensareen <viren.sareen@gmail.com>
2020-11-25 13:28:06 +0000

    VIH-6804 post endpoint create notification (#11)
    * added contract for AddNotificationRequest.cs
    
    * updated data types for notification req
    
    Co-authored-by: Viren Sareen <viren.sareen@solirius.com>

--- NotificationApi/NotificationApi.Contract/Requests/AddNotificationRequest.cs
+++ NotificationApi/NotificationApi.Contract/Requests/AddNotificationRequest.cs
@@ -1,3 +1,6 @@
+using System;
+using NotificationApi.Domain.Enums;
+
 namespace NotificationApi.Contract.Requests
 {
     /// <summary>
@@ -5,13 +8,13 @@ namespace NotificationApi.Contract.Requests
     /// </summary>
     public class AddNotificationRequest
     {
-        public string ExternalId { get; set; }
+        public Guid ExternalId { get; set; }
         public string ContactEmail { get; set; }
-        public int NotificationType { get; set; }
+        public NotificationType NotificationType { get; set; }
         public string PhoneNumber { get; set; }
         public string HearingId { get; set; }
         public string ParticipantId { get; set; }
-        public int MessageType { get; set; }
+        public MessageType MessageType { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
     }
