a8c3a4ae61da49aae56f35a720a69657b8da83d5
virensareen <viren.sareen@gmail.com>
2020-11-25 13:07:48 +0000

    added contract for AddNotificationRequest.cs (#10)
    Co-authored-by: Viren Sareen <viren.sareen@solirius.com>

--- NotificationApi/NotificationApi.Contract/Requests/AddNotificationRequest.cs
+++ NotificationApi/NotificationApi.Contract/Requests/AddNotificationRequest.cs
@@ -5,6 +5,14 @@ namespace NotificationApi.Contract.Requests
     /// </summary>
     public class AddNotificationRequest
     {
-        
+        public string ExternalId { get; set; }
+        public string ContactEmail { get; set; }
+        public int NotificationType { get; set; }
+        public string PhoneNumber { get; set; }
+        public string HearingId { get; set; }
+        public string ParticipantId { get; set; }
+        public int MessageType { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
     }
 }
