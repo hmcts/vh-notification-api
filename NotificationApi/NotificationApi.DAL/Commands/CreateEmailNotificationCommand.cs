99f91fd9f00ae38470101fe389c838774aceaae3
Viren Sareen <viren.sareen@solirius.com>
2020-11-30 12:38:33 +0000

    fixed integration test

--- NotificationApi/NotificationApi.DAL/Commands/CreateEmailNotificationCommand.cs
+++ NotificationApi/NotificationApi.DAL/Commands/CreateEmailNotificationCommand.cs
@@ -35,7 +35,9 @@ namespace NotificationApi.DAL.Commands
         public async Task Handle(CreateEmailNotificationCommand command) 
         {
             var notification = new EmailNotification((NotificationType)command.NotificationType, command.ContactEmail, command.ParticipantId, command.HearingId);
-            _notificationsApiDbContext.Notifications.Add(notification);
+            notification.AssignExternalId(string.Empty);
+            notification.AssignPayload(string.Empty);
+            await _notificationsApiDbContext.Notifications.AddAsync(notification);
             await _notificationsApiDbContext.SaveChangesAsync();
 
             command.NotificationId = notification.Id;
