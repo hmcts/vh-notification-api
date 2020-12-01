380184b454f2739765e262d3b009c9b98536a4b0
Viren Sareen <viren.sareen@solirius.com>
2020-12-01 10:54:16 +0000

    added conditional validation for messagetype

--- NotificationApi/NotificationApi/Validations/AddNotificationRequestValidation.cs
+++ NotificationApi/NotificationApi/Validations/AddNotificationRequestValidation.cs
@@ -18,16 +18,26 @@ namespace NotificationApi.Validations
         public AddNotificationRequestValidation()
         {
             RuleFor(x => x.Parameters).NotEmpty().WithMessage(MissingParametersMessage);
-            RuleFor(x => x.ContactEmail).NotEmpty().WithMessage(MissingEmailMessage);
+            RuleFor(x => x.ContactEmail).NotEmpty().When(IsEmail).WithMessage(MissingEmailMessage);
             RuleFor(x => x.HearingId).NotEmpty().WithMessage(MissingHearingIdMessage);
             RuleFor(x => x.MessageType).Must(ValidMessageType).WithMessage(InvalidMessageTypeMessage);
             RuleFor(x => x.NotificationType).Must(ValidNotificationType).WithMessage(InvalidNotificationTypeMessage);
             RuleFor(x => x.ParticipantId).NotEmpty().WithMessage(MissingParticipantIdMessage);
-            RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage(MissingPhoneNumberMessage);
+            RuleFor(x => x.PhoneNumber).NotEmpty().When(IsPhone).WithMessage(MissingPhoneNumberMessage);
         }
 
         private bool ValidNotificationType(int value) => Enum.IsDefined(typeof(NotificationType), value);
 
         private bool ValidMessageType(int value) => Enum.IsDefined(typeof(MessageType), value);
+
+        private bool IsEmail(AddNotificationRequest arg)
+        {
+            return arg.MessageType == (int) MessageType.Email;
+        }
+        
+        private bool IsPhone(AddNotificationRequest arg)
+        {
+            return arg.MessageType == (int) MessageType.SMS;
+        }
     }
 }
