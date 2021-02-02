edf525e63f935d64b3f64a752c503fe4ad9968b3
Levi <levi.butler@hmcts.net>
2021-02-02 15:16:31 +0000

    added configuration provider

--- NotificationApi/NotificationApi/Program.cs
+++ NotificationApi/NotificationApi/Program.cs
@@ -1,6 +1,7 @@
-﻿using System.IO;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using VH.Core.Configuration;
 
 namespace NotificationApi
 {
@@ -15,10 +16,10 @@ namespace NotificationApi
             CreateHostBuilder(args).Build().Run();
         }
 
-        // ReSharper disable once MemberCanBePrivate.Global Needed for client generation on build with nswag
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
+                .AddAksKeyVaultSecretProvider()
                 .ConfigureWebHostDefaults(webBuilder => {
                     webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
                     webBuilder.UseIISIntegration();
