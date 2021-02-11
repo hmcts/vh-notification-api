067dc721b2e259fff803016985139f9b1bc4cb89
David Decent <david@squidbox.co.uk>
2021-02-11 14:03:29 +0000

    VIH-7260  added https config setting and startup check

--- NotificationApi/NotificationApi/Startup.cs
+++ NotificationApi/NotificationApi/Startup.cs
@@ -28,6 +28,7 @@ namespace NotificationApi
         }
 
         private IConfiguration Configuration { get; }
+        public SettingsConfiguration SettingsConfiguration { get; private set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -63,6 +64,7 @@ namespace NotificationApi
 
         private void RegisterSettings(IServiceCollection services)
         {
+            SettingsConfiguration = Configuration.Get<SettingsConfiguration>();
             services.Configure<AzureAdConfiguration>(options => Configuration.Bind("AzureAd", options));
             services.Configure<ServicesConfiguration>(options => Configuration.Bind("Services", options));
             services.Configure<NotifyConfiguration>(options => Configuration.Bind("NotifyConfiguration", options));
@@ -121,7 +123,7 @@ namespace NotificationApi
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
+            else if(!SettingsConfiguration.DisableHttpsRedirection)
             {
                 app.UseHsts();
                 app.UseHttpsRedirection();
