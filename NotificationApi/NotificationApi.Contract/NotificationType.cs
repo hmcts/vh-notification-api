217c927eec79ec009028a4d9c21c92b42fa522f1
Shaed Parkar <shaed.parkar@slimsolutions.company>
2021-02-15 11:45:20 +0000

    Add additional notification types for reminder and amendment

--- NotificationApi/NotificationApi.Contract/NotificationType.cs
+++ NotificationApi/NotificationApi.Contract/NotificationType.cs
@@ -9,6 +9,13 @@ namespace NotificationApi.Contract
         HearingConfirmationLip = 4,
         HearingConfirmationRepresentative = 5,
         HearingConfirmationJudge = 6,
-        HearingConfirmationJoh = 7
+        HearingConfirmationJoh = 7,
+        HearingAmendmentLip = 8,
+        HearingAmendmentRepresentative = 9,
+        HearingAmendmentJudge = 10,
+        HearingAmendmentJoh = 11,
+        HearingReminderLip = 12,
+        HearingReminderRepresentative = 13,
+        HearingReminderJoh = 14
     }
 }
