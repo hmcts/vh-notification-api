81d5256d4875d6be4c0cd0a9203f4cb88049d3ef
Prasanna Dommalapati <prasanna.dommalapati@gmail.com>
2021-04-28 16:20:26 +0100

    VIH-7539 New notification type migration AddEJudJohNotifications

---
+++ NotificationApi/NotificationApi.DAL/Migrations/20210428150118_AddEJudJohNotifications.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+using NotificationApi.Domain;
+using NotificationApi.Domain.Enums;
+
+namespace NotificationApi.DAL.Migrations
+{
+    public partial class AddEJudJohNotifications : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+               nameof(Template),
+               new[] { "NotifyTemplateId", "NotificationType", "MessageType", "Parameters" },
+               new object[,]
+               {
+                    {
+                        "ca353650-c43b-46e4-890c-b8dad47a825a", (int) NotificationType.HearingAmendmentEJudJoh,
+                        (int) MessageType.Email,
+                        "case name,case number,judge,day month year,time"
+                    }
+               });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                nameof(Template),
+                "NotificationType",
+                (int)NotificationType.HearingAmendmentEJudJoh
+            );
+        }
+    }
+}
