0c4eedab59797254d7dcd9a97e3f390190b08377
Christopher Porter <christopher.porter@version1.com>
2021-12-13 19:33:35 +0000

    VIH-8482 notify not sending emails to users booked on to a confirmed hearing

---
+++ NotificationApi/NotificationApi.UnitTests/Services/NotifyServiceTests.cs.cs
@@ -0,0 +1,112 @@
+using AdminWebsite.Services;
+using Autofac.Extras.Moq;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NotificationApi.DAL.Commands;
+using NotificationApi.DAL.Commands.Core;
+using NotificationApi.DAL.Queries;
+using NotificationApi.DAL.Queries.Core;
+using NotificationApi.Domain;
+using NotificationApi.Domain.Enums;
+using NotificationApi.Services;
+using Notify.Interfaces;
+using Notify.Models.Responses;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+using System.Threading.Tasks;
+
+namespace NotificationApi.UnitTests.Services
+{
+    public class NotifyServiceTests
+    {
+        private Mock<IPollyRetryService> _pollyRetryService;
+        private Mock<ILogger<CreateNotificationService>> _loggerMock;
+        private Mock<IAsyncNotificationClient> _asyncNotificationClient;
+        protected Mock<ICommandHandler> _commandHandlerMock;
+        protected Mock<IQueryHandler> _queryHandlerMock;
+        protected Template _template;
+        protected CreateEmailNotificationCommand _createEmailNotificationCommand;
+        protected GetTemplateByNotificationTypeQuery _query;
+        private AutoMock _mocker;        
+        private EmailResponseContent _emailResponseContent;
+        private EmailNotificationResponse _expectedEmailNotificationResponse;
+        private Dictionary<string, dynamic> _parameters;
+        
+
+
+        private CreateNotificationService _createNotificationService;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _mocker = AutoMock.GetLoose();
+            _loggerMock = new Mock<ILogger<CreateNotificationService>>();
+            _queryHandlerMock = new Mock<IQueryHandler>();
+            _commandHandlerMock = new Mock<ICommandHandler>();
+            _asyncNotificationClient = new Mock<IAsyncNotificationClient>();
+            _pollyRetryService = new Mock<IPollyRetryService>();
+
+            _createNotificationService = new CreateNotificationService(
+              _commandHandlerMock.Object,
+              _asyncNotificationClient.Object,
+              _queryHandlerMock.Object,
+              _pollyRetryService.Object,
+              _loggerMock.Object
+              );
+
+            _emailResponseContent = new EmailResponseContent()
+            {
+                body = "Email reponse Body"
+            };
+            
+            _template = new Template(Guid.NewGuid(), NotificationType.JudgeDemoOrTest, MessageType.Email, "param1, param2");
+            _createEmailNotificationCommand = new CreateEmailNotificationCommand(NotificationType.JudgeDemoOrTest, "testemail@gmail.com", Guid.NewGuid(), Guid.NewGuid());
+            _parameters = new Dictionary<string, dynamic>
+            {
+                { "Test param1Key", "Test param1Value" }
+            };
+            _emailResponseContent = new EmailResponseContent()
+            {
+                body = "Email reponse Body"
+            };
+            _expectedEmailNotificationResponse = new EmailNotificationResponse()
+            {
+                id = Guid.NewGuid().ToString(),
+                reference = "reference",
+                uri = "uri",
+                content = _emailResponseContent,
+                template = null
+            };          
+        }
+
+        [Test]
+        public async Task Should_return_response_from_send_email()
+        {
+            
+        _pollyRetryService.Setup(x => x.WaitAndRetryAsync<Exception, EmailNotificationResponse>
+            (
+                It.IsAny<int>(), It.IsAny<Func<int, TimeSpan>>(), It.IsAny<Action<int>>(), It.IsAny<Func<EmailNotificationResponse, bool>>(), It.IsAny<Func<Task<EmailNotificationResponse>>>()
+            ))
+            .Callback(async (int retries, Func<int, TimeSpan> sleepDuration, Action<int> retryAction, Func<EmailNotificationResponse, bool> handleResultCondition, Func<Task<EmailNotificationResponse>> executeFunction) =>
+            {
+                sleepDuration(1);
+                retryAction(7);
+                handleResultCondition(_expectedEmailNotificationResponse);
+                await executeFunction();
+            })
+            .ReturnsAsync(_expectedEmailNotificationResponse);
+
+            _mocker.Mock<IAsyncNotificationClient>()
+               .Setup(x => x.SendEmailAsync(_createEmailNotificationCommand.ContactEmail, _template.NotifyTemplateId.ToString(), _parameters, null, null)).ReturnsAsync(_expectedEmailNotificationResponse);
+          
+            var result = await _createNotificationService.SendEmailAsyncRetry(_createEmailNotificationCommand.ContactEmail, _template.NotifyTemplateId.ToString(), _parameters, null);
+
+            result.Should().NotBeNull();
+            result.Should().BeEquivalentTo(_expectedEmailNotificationResponse);
+        }      
+    }
+}
---
+++ NotificationApi/NotificationApi.UnitTests/Services/PollyRetryServiceTests.cs
@@ -0,0 +1,108 @@
+using AdminWebsite.Services;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace NotificationApi.UnitTests.Services
+{
+    public class PollyRetryServiceTests
+    {
+        private readonly IPollyRetryService _pollyRetryService;
+
+        public PollyRetryServiceTests()
+        {
+            _pollyRetryService = new PollyRetryService();
+        }
+
+        [Test]
+        public void WaitAndRetryAsync_Retries_On_Exception()
+        {
+            var retryInvoked = false;
+
+            _pollyRetryService.WaitAndRetryAsync<Exception, object>
+            (
+                3, i => TimeSpan.FromMilliseconds(1), retryAttempt => retryInvoked = true,
+#pragma warning disable S3626 // Jump statements should not be redundant
+                () => throw new Exception("What")
+#pragma warning restore S3626 // Jump statements should not be redundant
+            );
+
+            Assert.True(retryInvoked);
+        }
+
+        [Test]
+        public async Task WaitAndRetryAsync_Does_Not_Retry()
+        {
+            var retryInvoked = false;
+
+            var result = await _pollyRetryService.WaitAndRetryAsync<Exception, object>
+            (
+                3, i => TimeSpan.FromMilliseconds(1), retryAttempt => retryInvoked = true,
+                () => Task.FromResult<object>("returned")
+            );
+
+            Assert.False(retryInvoked);
+            result.Should().Be("returned");
+        }
+
+        [Test]
+        public async Task WaitAndRetryAsync_With_Result_Retries_On_Exception()
+        {
+            var retryInvoked = false;
+
+            try
+            {
+                await _pollyRetryService.WaitAndRetryAsync<Exception, EmailNotificationResponseTest>
+                (
+                    3, i => TimeSpan.FromMilliseconds(1), retryAttempt => retryInvoked = true,
+                    x => !x.Success,
+#pragma warning disable S3626 // Jump statements should not be redundant
+                    () => throw new Exception("What")
+#pragma warning restore S3626 // Jump statements should not be redundant
+                );
+            }
+            catch
+            {
+                Assert.True(retryInvoked);
+            }
+        }
+
+        [Test]
+        public async Task WaitAndRetryAsync_With_Result_Retries_On_Failed_Result()
+        {
+            var retryInvoked = false;
+
+            await _pollyRetryService.WaitAndRetryAsync<Exception, EmailNotificationResponseTest>
+            (
+                3, i => TimeSpan.FromMilliseconds(1), retryAttempt => retryInvoked = true,
+                x => !x.Success,
+                () => Task.FromResult(new EmailNotificationResponseTest { Success = false })
+            );
+
+            Assert.True(retryInvoked);
+        }
+
+        [Test]
+        public async Task WaitAndRetryAsync_With_Result_Does_Not_Retry()
+        {
+            var retryInvoked = false;
+
+            var result = await _pollyRetryService.WaitAndRetryAsync<Exception, EmailNotificationResponseTest>
+            (
+                3, i => TimeSpan.FromMilliseconds(1), retryAttempt => retryInvoked = true,
+                x => !x.Success,
+                () => Task.FromResult(new EmailNotificationResponseTest { Success = true })
+            );
+
+            Assert.False(retryInvoked);
+
+            result.Success.Should().BeTrue();
+        }
+
+        private class EmailNotificationResponseTest
+        {
+            public bool Success { get; set; }
+        }
+    }
+}
