1138b997510fb08fdb640da0592ab96835074911
Christopher Pateman <christopher.pateman@version1.com>
2022-04-08 08:32:23 +0100

    Add EF Publishing from Pipeline

--- NotificationApi/NotificationApi/Startup.cs
+++ NotificationApi/NotificationApi/Startup.cs
@@ -110,9 +110,7 @@ namespace NotificationApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.RunLatestMigrations();
-            
+        {            
             app.UseOpenApi();
             app.UseSwaggerUi3(c =>
             {
