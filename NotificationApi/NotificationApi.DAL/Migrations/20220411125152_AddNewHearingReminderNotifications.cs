85a9ae14b272c4711682ecebcef7680a7f6beb64
muktadiru <muktadiru.ahmed@google.com>
2022-04-11 14:45:41 +0100

    added new migration for new confirmation emails

---
+++ NotificationApi/NotificationApi.DAL/Migrations/20220411125152_AddNewHearingReminderNotifications.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NotificationApi.Domain;
+using NotificationApi.Domain.Enums;
+
+namespace NotificationApi.DAL.Migrations
+{
+    public partial class AddNewHearingReminderNotifications : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var templateFactory = NotifyTemplateFactory.Get();
+
+            migrationBuilder.InsertData(
+                nameof(Template),
+                new[] { "NotifyTemplateId", "NotificationType", "MessageType", "Parameters" },
+                new object[,]
+                {
+                    {
+                        templateFactory.NewHearingReminderLIP, (int) NotificationType.NewHearingReminderLIP,
+                        (int) MessageType.Email,
+                        "case name, case number, name, day month year, time"
+                    },
+                    {
+                        templateFactory.NewHearingReminderRepresentative, (int) NotificationType.NewHearingReminderRepresentative,
+                        (int) MessageType.Email,
+                        "case name, case number, client name, solicitor name, day month year, time"
+                    },
+                    {
+                        templateFactory.NewHearingReminderJOH, (int) NotificationType.NewHearingReminderJOH,
+                        (int) MessageType.Email,
+                        "case name, case number, judicial office holder, day month year, time"
+                    },
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                nameof(Template),
+                "NotificationType",
+                (int)NotificationType.NewHearingReminderLIP
+            );
+
+            migrationBuilder.DeleteData(
+                nameof(Template),
+                "NotificationType",
+                (int)NotificationType.NewHearingReminderRepresentative
+            );
+            migrationBuilder.DeleteData(
+                nameof(Template),
+                "NotificationType",
+                (int)NotificationType.NewHearingReminderJOH
+            );
+
+        }
+    }
+}
