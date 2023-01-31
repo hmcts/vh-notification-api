50267de42611bfe87f28fd8c180c1c4a31a8c5b5
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-01-31 21:54:06 +0000

    detect duplicate templates and throw meaningful error

---
+++ NotificationApi/NotificationApi.DAL/Exceptions/DuplicateNotificationTemplateException.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Runtime.Serialization;
+using NotificationApi.Domain.Enums;
+
+namespace NotificationApi.DAL.Exceptions;
+
+[Serializable]
+public class DuplicateNotificationTemplateException : Exception
+{
+    public DuplicateNotificationTemplateException(NotificationType notificationType) : base(
+        $"Duplicate entry for notification type {notificationType} found")
+    {
+    }
+        
+    protected DuplicateNotificationTemplateException(SerializationInfo info, StreamingContext context) 
+        : base(info, context)
+    {
+    }
+}
