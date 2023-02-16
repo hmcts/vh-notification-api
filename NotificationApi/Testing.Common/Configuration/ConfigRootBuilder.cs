56bce94edcbf3fcf4140b70dcb7394d051caee9d
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-02-16 10:39:13 +0000

    Update database tests setup to run migrations first

---
+++ NotificationApi/Testing.Common/Configuration/ConfigRootBuilder.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.Configuration;
+
+namespace Testing.Common.Configuration;
+
+public static class ConfigRootBuilder
+{
+    private const string UserSecretId = "4E35D845-27E7-4A19-BE78-CDA896BF907D";
+    public static IConfigurationRoot Build(string userSecretId = UserSecretId)
+    {
+        return new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddUserSecrets(userSecretId)
+            .AddEnvironmentVariables()
+            .Build();
+    }
+}
