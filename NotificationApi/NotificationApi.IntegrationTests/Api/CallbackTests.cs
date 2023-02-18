416e315044f38705c8b328bdf9b349722041cf5d
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-02-18 10:00:54 +0000

    use fake token as the default authentication in the test server

--- NotificationApi/NotificationApi.IntegrationTests/Api/CallbackTests.cs
+++ NotificationApi/NotificationApi.IntegrationTests/Api/CallbackTests.cs
@@ -13,88 +13,89 @@ using NotificationApi.Validations;
 using NUnit.Framework;
 using Testing.Common.Helper;
 
-namespace NotificationApi.IntegrationTests.Api;
-
-public class CallbackTests : ApiTest
+namespace NotificationApi.IntegrationTests.Api
 {
-    [Test]
-    public async Task should_update_the_delivery_status_a_notification()
+    public class CallbackTests : ApiTest
     {
-        // arrange
-        var deliveryStatus = "delivered";
-        var notification = await TestDataManager.SeedSendingNotification();
-        var request = BuildRequest(notification, deliveryStatus);
-
-        // act
-        using var client = Application.CreateClient();
-        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", GenerateCallbackToken());
-        var result = await client.PostAsync(
-            ApiUriFactory.NotificationEndpoints.UpdateNotification, RequestBody.Set(request));
-
-        // assert
-        result.IsSuccessStatusCode.Should().BeTrue();
-    }
-
-    [Test]
-    public async Task should_return_bad_request_when_the_external_id_does_not_match()
-    {
-        // arrange
-        var deliveryStatus = "delivered";
-        var notification = await TestDataManager.SeedSendingNotification();
-        var request = BuildRequest(notification, deliveryStatus);
-        request.Id = Guid.NewGuid().ToString();
-
-        // act
-        using var client = Application.CreateClient();
-        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", GenerateCallbackToken());
-        var result = await client.PostAsync(
-            ApiUriFactory.NotificationEndpoints.UpdateNotification, RequestBody.Set(request));
-
-        // assert
-        result.IsSuccessStatusCode.Should().BeFalse();
-        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-        var validationProblemDetails = await ApiClientResponse.GetResponses<ValidationProblemDetails>(result.Content);
-        validationProblemDetails.Errors["database"][0].Should()
-            .Be($"ExternalId {request.Id} does not belong to Notification {notification.Id}");
-    }
-
-    [Test]
-    public async Task should_return_bad_request_when_invalid_payload_is_sent()
-    {
-        // arrange
-        var deliveryStatus = "delivered";
-        var notification = await TestDataManager.SeedSendingNotification();
-        var request = BuildRequest(notification, deliveryStatus);
-        request.Id = string.Empty;
-
-        // act
-        using var client = Application.CreateClient();
-        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", GenerateCallbackToken());
-        var result = await client.PostAsync(
-            ApiUriFactory.NotificationEndpoints.UpdateNotification, RequestBody.Set(request));
-
-        // assert
-        result.IsSuccessStatusCode.Should().BeFalse();
-        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-        var validationProblemDetails = await ApiClientResponse.GetResponses<ValidationProblemDetails>(result.Content);
-        validationProblemDetails.Errors[nameof(request.Id)][0].Should()
-            .Be(NotificationCallbackRequestValidation.MissingIdMessage);
-    }
-    
-    [TearDown]
-    public async Task TearDown()
-    {
-        await TestDataManager.RemoveNotifications(TestDataManager.NotificationsCreated.Select(x => x.Id));
-        TestDataManager.NotificationsCreated.Clear();
-    }
-    
-    private NotificationCallbackRequest BuildRequest(Notification notification, string deliveryStatus)
-    {
-        return new NotificationCallbackRequest
+        [Test]
+        public async Task should_update_the_delivery_status_a_notification()
         {
-            Id = notification.ExternalId,
-            Reference = notification.Id.ToString(),
-            Status = deliveryStatus
-        };
+            // arrange
+            var deliveryStatus = "delivered";
+            var notification = await TestDataManager.SeedSendingNotification();
+            var request = BuildRequest(notification, deliveryStatus);
+
+            // act
+            using var client = Application.CreateClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", GenerateCallbackToken());
+            var result = await client.PostAsync(
+                ApiUriFactory.NotificationEndpoints.UpdateNotification, RequestBody.Set(request));
+
+            // assert
+            result.IsSuccessStatusCode.Should().BeTrue();
+        }
+
+        [Test]
+        public async Task should_return_bad_request_when_the_external_id_does_not_match()
+        {
+            // arrange
+            var deliveryStatus = "delivered";
+            var notification = await TestDataManager.SeedSendingNotification();
+            var request = BuildRequest(notification, deliveryStatus);
+            request.Id = Guid.NewGuid().ToString();
+
+            // act
+            using var client = Application.CreateClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", GenerateCallbackToken());
+            var result = await client.PostAsync(
+                ApiUriFactory.NotificationEndpoints.UpdateNotification, RequestBody.Set(request));
+
+            // assert
+            result.IsSuccessStatusCode.Should().BeFalse();
+            result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            var validationProblemDetails = await ApiClientResponse.GetResponses<ValidationProblemDetails>(result.Content);
+            validationProblemDetails.Errors["database"][0].Should()
+                .Be($"ExternalId {request.Id} does not belong to Notification {notification.Id}");
+        }
+
+        [Test]
+        public async Task should_return_bad_request_when_invalid_payload_is_sent()
+        {
+            // arrange
+            var deliveryStatus = "delivered";
+            var notification = await TestDataManager.SeedSendingNotification();
+            var request = BuildRequest(notification, deliveryStatus);
+            request.Id = string.Empty;
+
+            // act
+            using var client = Application.CreateClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", GenerateCallbackToken());
+            var result = await client.PostAsync(
+                ApiUriFactory.NotificationEndpoints.UpdateNotification, RequestBody.Set(request));
+
+            // assert
+            result.IsSuccessStatusCode.Should().BeFalse();
+            result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            var validationProblemDetails = await ApiClientResponse.GetResponses<ValidationProblemDetails>(result.Content);
+            validationProblemDetails.Errors[nameof(request.Id)][0].Should()
+                .Be(NotificationCallbackRequestValidation.MissingIdMessage);
+        }
+    
+        [TearDown]
+        public async Task TearDown()
+        {
+            await TestDataManager.RemoveNotifications(TestDataManager.NotificationsCreated.Select(x => x.Id));
+            TestDataManager.NotificationsCreated.Clear();
+        }
+    
+        private NotificationCallbackRequest BuildRequest(Notification notification, string deliveryStatus)
+        {
+            return new NotificationCallbackRequest
+            {
+                Id = notification.ExternalId,
+                Reference = notification.Id.ToString(),
+                Status = deliveryStatus
+            };
+        }
     }
 }
--- NotificationApi/NotificationApi.IntegrationTests/Api/CreateHearingNotificationTests.cs
+++ NotificationApi/NotificationApi.IntegrationTests/Api/CreateHearingNotificationTests.cs
@@ -13,84 +13,85 @@ using NUnit.Framework;
 using Testing.Common.Extensions;
 using Testing.Common.Helper;
 
-namespace NotificationApi.IntegrationTests.Api;
-
-public class CreateHearingNotificationTests : ApiTest
+namespace NotificationApi.IntegrationTests.Api
 {
-    [Test]
-    public async Task should_create_notification()
+    public class CreateHearingNotificationTests : ApiTest
     {
-        // arrange
-        var request = BuildNewUserNotificationRequest(MessageType.Email, NotificationType.CreateIndividual);
-
-        // act
-        using var client = Application.CreateClient();
-        var result = await client.PostAsync(
-            ApiUriFactory.NotificationEndpoints.CreateNewEmailNotification, RequestBody.Set(request));
-
-
-        // assert
-        result.IsSuccessStatusCode.Should().BeTrue();
-
-        var notifications = await TestDataManager.GetNotifications(request.HearingId.Value, request.ParticipantId.Value,
-            (Domain.Enums.NotificationType) request.NotificationType,
-            request.ContactEmail);
-        notifications.Count.Should().Be(1);
-    }
-
-    [Test]
-    public async Task should_create_notification_once_when_duplicate_request_is_sent()
-    {
-        // arrange
-        var request = BuildNewUserNotificationRequest(MessageType.Email, NotificationType.CreateIndividual);
-
-        // act
-        using var client = Application.CreateClient();
-        var result1 = await client.PostAsync(
-            ApiUriFactory.NotificationEndpoints.CreateNewEmailNotification, RequestBody.Set(request));
-        var result2 = await client.PostAsync(
-            ApiUriFactory.NotificationEndpoints.CreateNewEmailNotification, RequestBody.Set(request));
-
-        // assert
-        result1.IsSuccessStatusCode.Should().BeTrue();
-        result2.IsSuccessStatusCode.Should().BeTrue();
-
-        var notifications = await TestDataManager.GetNotifications(request.HearingId.Value, request.ParticipantId.Value,
-            (Domain.Enums.NotificationType) request.NotificationType,
-            request.ContactEmail);
-        notifications.Count.Should().Be(1);
-    }
-
-    [Test]
-    public async Task should_return_bad_request_when_an_invalid_payload_is_sent()
-    {
-        // arrange
-        var request = BuildNewUserNotificationRequest(MessageType.Email, NotificationType.CreateIndividual);
-        request.ContactEmail = null;
-
-        // act
-        using var client = Application.CreateClient();
-        var result = await client.PostAsync(
-            ApiUriFactory.NotificationEndpoints.CreateNewEmailNotification, RequestBody.Set(request));
-
-        // assert
-        result.IsSuccessStatusCode.Should().BeFalse();
-        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-        var validationProblemDetails = await ApiClientResponse.GetResponses<ValidationProblemDetails>(result.Content);
-        validationProblemDetails.Errors[nameof(request.ContactEmail)][0].Should()
-            .Be(AddNotificationRequestValidation.MissingEmailMessage);
-    }
-
-    private AddNotificationRequest BuildNewUserNotificationRequest(MessageType messageType,
-        NotificationType notificationType)
-    {
-        var parameters = new Dictionary<string, string>
+        [Test]
+        public async Task should_create_notification()
         {
-            {"name", $"{Faker.Name.FullName()}"},
-            {"username", $"{Guid.NewGuid()}@intautomation.com"},
-            {"random password", "inttestpassword!"}
-        };
+            // arrange
+            var request = BuildNewUserNotificationRequest(MessageType.Email, NotificationType.CreateIndividual);
 
-        return AddNotificationRequestBuilder.BuildRequest(messageType, notificationType, parameters);
+            // act
+            using var client = Application.CreateClient();
+            var result = await client.PostAsync(
+                ApiUriFactory.NotificationEndpoints.CreateNewEmailNotification, RequestBody.Set(request));
+
+
+            // assert
+            result.IsSuccessStatusCode.Should().BeTrue();
+
+            var notifications = await TestDataManager.GetNotifications(request.HearingId.Value, request.ParticipantId.Value,
+                (Domain.Enums.NotificationType) request.NotificationType,
+                request.ContactEmail);
+            notifications.Count.Should().Be(1);
+        }
+
+        [Test]
+        public async Task should_create_notification_once_when_duplicate_request_is_sent()
+        {
+            // arrange
+            var request = BuildNewUserNotificationRequest(MessageType.Email, NotificationType.CreateIndividual);
+
+            // act
+            using var client = Application.CreateClient();
+            var result1 = await client.PostAsync(
+                ApiUriFactory.NotificationEndpoints.CreateNewEmailNotification, RequestBody.Set(request));
+            var result2 = await client.PostAsync(
+                ApiUriFactory.NotificationEndpoints.CreateNewEmailNotification, RequestBody.Set(request));
+
+            // assert
+            result1.IsSuccessStatusCode.Should().BeTrue();
+            result2.IsSuccessStatusCode.Should().BeTrue();
+
+            var notifications = await TestDataManager.GetNotifications(request.HearingId.Value, request.ParticipantId.Value,
+                (Domain.Enums.NotificationType) request.NotificationType,
+                request.ContactEmail);
+            notifications.Count.Should().Be(1);
+        }
+
+        [Test]
+        public async Task should_return_bad_request_when_an_invalid_payload_is_sent()
+        {
+            // arrange
+            var request = BuildNewUserNotificationRequest(MessageType.Email, NotificationType.CreateIndividual);
+            request.ContactEmail = null;
+
+            // act
+            using var client = Application.CreateClient();
+            var result = await client.PostAsync(
+                ApiUriFactory.NotificationEndpoints.CreateNewEmailNotification, RequestBody.Set(request));
+
+            // assert
+            result.IsSuccessStatusCode.Should().BeFalse();
+            result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            var validationProblemDetails = await ApiClientResponse.GetResponses<ValidationProblemDetails>(result.Content);
+            validationProblemDetails.Errors[nameof(request.ContactEmail)][0].Should()
+                .Be(AddNotificationRequestValidation.MissingEmailMessage);
+        }
+
+        private AddNotificationRequest BuildNewUserNotificationRequest(MessageType messageType,
+            NotificationType notificationType)
+        {
+            var parameters = new Dictionary<string, string>
+            {
+                {"name", $"{Faker.Name.FullName()}"},
+                {"username", $"{Guid.NewGuid()}@intautomation.com"},
+                {"random password", "inttestpassword!"}
+            };
+
+            return AddNotificationRequestBuilder.BuildRequest(messageType, notificationType, parameters);
+        }
     }
 }
