a454f374600ffeb6cac5f6372689aa19f6ad40c2
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-02-23 16:38:37 +0000

    random commit

--- scripts/tests/run-tests.sh
+++ scripts/tests/run-tests.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 set -x
 
 dotnet sonarscanner begin /k:"${SONAR_PROJECT_KEY}" /o:"${SONAR_ORG}" /version:"${SONAR_PROJECT_VERSION}" /name:"${SONAR_PROJECT_NAME}" /d:sonar.host.url="${SONAR_HOST}" /d:sonar.login="${SONAR_TOKEN}" /d:sonar.cs.opencover.reportsPaths="${PWD}/Coverage/coverage.opencover.xml" /d:sonar.coverage.exclusions="**/NotificationApi/Swagger/**/*,**/Program.cs,**/Startup.cs,**/Testing.Common/**/*,**/NotificationApi.Common/**/*,**/NotificationApi.IntegrationTests/**/*,**/NotificationApi.UnitTests/**/*,**/NotificationApi/Extensions/*,**/NotificationApi.DAL/Migrations/**/*" /d:sonar.cpd.exclusions="**/Program.cs,**/NotificationType.cs,**/Startup.cs,**/Testing.Common/**/*,**/NotificationApi/Swagger/**/*,NotificationApi/NotificationApi.DAL/Migrations/*,NotificationApi/NotificationApi.DAL/TemplateDataForEnvironments.cs"
