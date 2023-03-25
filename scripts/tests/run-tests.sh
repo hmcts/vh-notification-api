722e81c2d056126b062c27a15c2141529330d474
v1ferrarij <94917970+v1ferrarij@users.noreply.github.com>
2023-03-25 10:34:19 +0000

    Update run-tests.sh

--- scripts/tests/run-tests.sh
+++ scripts/tests/run-tests.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -x
 
+git clone https://github.com/hmcts/vh-notification-api.git /app/vh-notification-api
+cd /app/vh-notification-api
+git checkout VIH-9999-SonarIssue
+
 rm -d -r ${PWD}/Coverage
 rm -d -r ${PWD}/TestResults
 
@@ -9,16 +13,16 @@ dotnet sonarscanner begin /k:"${SONAR_PROJECT_KEY}" /o:"${SONAR_ORG}" /version:"
 exclusions="[Testing.Common]*,[NotificationApi.Common]NotificationApi.Common.*,[NotificationApi.Domain]*.Ddd*,[NotificationApi.DAL]*.Migrations*"
 configuration=Release
 
-dotnet build NotificationApi/NotificationApi.sln -c $configuration
+dotnet build /app/vh-notification-api/NotificationApi/NotificationApi.sln -c $configuration
 # Script is for docker compose tests where the script is at the root level
-dotnet test NotificationApi/NotificationApi.UnitTests/NotificationApi.UnitTests.csproj -c $configuration --no-build --results-directory ./TestResults --logger "trx;LogFileName=NotificationApi-Unit-Tests-TestResults.trx" \
+dotnet test /app/vh-notification-api/NotificationApi/NotificationApi.UnitTests/NotificationApi.UnitTests.csproj -c $configuration --no-build --results-directory ./TestResults --logger "trx;LogFileName=NotificationApi-Unit-Tests-TestResults.trx" \
     "/p:CollectCoverage=true" \
     "/p:Exclude=\"${exclusions}\"" \
     "/p:CoverletOutput=${PWD}/Coverage/" \
     "/p:MergeWith=${PWD}/Coverage/coverage.json" \
     "/p:CoverletOutputFormat=\"opencover,json,cobertura,lcov\""
 
-dotnet test NotificationApi/NotificationApi.IntegrationTests/NotificationApi.IntegrationTests.csproj -c $configuration --no-build --results-directory ./TestResults --logger "trx;LogFileName=NotificationApi-Integration-Tests-TestResults.trx" \
+dotnet test /app/vh-notification-api/NotificationApi/NotificationApi.IntegrationTests/NotificationApi.IntegrationTests.csproj -c $configuration --no-build --results-directory ./TestResults --logger "trx;LogFileName=NotificationApi-Integration-Tests-TestResults.trx" \
     "/p:CollectCoverage=true" \
     "/p:Exclude=\"${exclusions}\"" \
     "/p:CoverletOutput=${PWD}/Coverage/" \
