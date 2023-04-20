2c0ade3af1def560a6314a04b893ce8834252836
shaed-parkar <41630528+shaed-parkar@users.noreply.github.com>
2023-04-20 09:45:16 +0100

    VIH-9849 fix incorrect parsing of dotnet paths (#127)

--- scripts/tests/run-tests.sh
+++ scripts/tests/run-tests.sh
@@ -1,22 +1,18 @@
 #!/bin/sh
 set -x
 
-rm -d -r ${PWD}/Coverage
-rm -d -r ${PWD}/TestResults
-
 exclusions="[Testing.Common]*,[NotificationApi.Common]NotificationApi.Common.*,[NotificationApi.Domain]*.Ddd*,[NotificationApi.DAL]*.Migrations*"
 configuration=Release
 
-dotnet build NotificationApi/NotificationApi.sln -c $configuration
 # Script is for docker compose tests where the script is at the root level
-dotnet test NotificationApi/NotificationApi.UnitTests/NotificationApi.UnitTests.csproj -c $configuration --no-build --results-directory ./TestResults --logger "trx;LogFileName=NotificationApi-Unit-Tests-TestResults.trx" \
+dotnet test NotificationApi/NotificationApi.UnitTests/NotificationApi.UnitTests.csproj -c $configuration --results-directory ./TestResults --logger "trx;LogFileName=NotificationApi-Unit-Tests-TestResults.trx" \
     "/p:CollectCoverage=true" \
     "/p:Exclude=\"${exclusions}\"" \
     "/p:CoverletOutput=${PWD}/Coverage/" \
     "/p:MergeWith=${PWD}/Coverage/coverage.json" \
     "/p:CoverletOutputFormat=\"opencover,json,cobertura,lcov\""
 
-dotnet test NotificationApi/NotificationApi.IntegrationTests/NotificationApi.IntegrationTests.csproj -c $configuration --no-build --results-directory ./TestResults --logger "trx;LogFileName=NotificationApi-Integration-Tests-TestResults.trx" \
+dotnet test NotificationApi/NotificationApi.IntegrationTests/NotificationApi.IntegrationTests.csproj -c $configuration --results-directory ./TestResults --logger "trx;LogFileName=NotificationApi-Integration-Tests-TestResults.trx" \
     "/p:CollectCoverage=true" \
     "/p:Exclude=\"${exclusions}\"" \
     "/p:CoverletOutput=${PWD}/Coverage/" \
