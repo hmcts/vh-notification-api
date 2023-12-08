38c66893f86aeea9f6e550f3d7a65dd45c291999
marcogagliardi <marco.gagliardi@justice.gov.uk>
2023-12-08 09:57:47 +0000

    VIH-10353 - Admin web Unable to sent notifications for Edit hearings (#149)
    * Update NotificationParameterMapper.cs
    
    * added test
    
    * Update MapToHearingAmendmentNotificationTests.cs
    
    * Update MapToHearingAmendmentNotificationTests.cs

---
+++ NotificationApi/NotificationApi.UnitTests/Mappers/NotificationParameterMapper/MapToHearingAmendmentNotificationTests.cs
@@ -0,0 +1,50 @@
+using System;
+using FluentAssertions;
+using NotificationApi.Common;
+using NotificationApi.Contract.Requests;
+using NUnit.Framework;
+
+namespace NotificationApi.UnitTests.Mappers.NotificationParameterMapper
+{
+    public class MapToHearingAmendmentNotificationTests
+    {
+
+        [Test]
+        public void should_map_to_amendment_notification_for_individual()
+        {
+            //Arrange
+            var oldDate = new DateTime(2020, 2, 10, 11, 30, 0, DateTimeKind.Utc);
+            var newDate = new DateTime(2020, 10, 12, 13, 10, 0, DateTimeKind.Utc);
+
+            var request = new HearingAmendmentRequest
+            {
+                HearingId = new Guid("73f2053e-74f1-4d6c-b817-246f4b22e666"),
+                ContactEmail = "Automation_226153990@hmcts.net",
+                ParticipantId = new Guid("73f2053e-74f1-4d6c-b817-246f4b22e665"),
+                CaseName = "Case name",
+                PreviousScheduledDateTime = oldDate,
+                NewScheduledDateTime = newDate,
+                RoleName = "Individual",
+                CaseNumber = "Original Hearing",
+                Name = $"Automation_FirstName Automation_LastName",
+                DisplayName = "Automation_FirstName Automation_LastName",
+                Representee = "",
+                Username = "Automation_338564597@hmcts.net"
+            };
+            
+            //Act
+            var result = NotificationApi.Services.NotificationParameterMapper.MapToHearingAmendment(request);
+
+            //Assert
+            result.Should().NotBeNull();
+            result[NotifyParams.CaseName].Should().Be("Case name");
+            result[NotifyParams.UserName].Should().Be("automation_338564597@hmcts.net");
+            result[NotifyParams.CaseNumber].Should().Be("Original Hearing");
+            result[NotifyParams.OldTime].Should().Be("11:30 AM");
+            result[NotifyParams.NewTime].Should().Be("2:10 PM");
+            result[NotifyParams.OldDayMonthYear].Should().Be("10 February 2020");
+            result[NotifyParams.NewDayMonthYear].Should().Be("12 October 2020");
+            result[NotifyParams.Name].Should().Be("Automation_FirstName Automation_LastName");
+        }
+    }
+}
