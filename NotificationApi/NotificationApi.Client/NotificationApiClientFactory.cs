2e62183ce5241ea750a55d68c4d164b1af1a2e72
Shaed Parkar <shaed.parkar@slimsolutions.tech>
2025-01-16 15:13:52 +0000

    Exclude client factory from code coverage check

--- NotificationApi/NotificationApi.Client/NotificationApiClientFactory.cs
+++ NotificationApi/NotificationApi.Client/NotificationApiClientFactory.cs
@@ -1,9 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Net.Http;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace NotificationApi.Client
 {
+    [ExcludeFromCodeCoverage]
     public partial class NotificationApiClient
     {
         public static NotificationApiClient GetClient(HttpClient httpClient)
